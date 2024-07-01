353b5a3cb23d351736a566a57a3a9181297adfbd
Clemens Blab <clemens@datenvorsprung.at>
2024-07-01 17:02:09 +0200

    Added more types of randomization in setup yaml file

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/cartpole_decoupled/randomizer_cartpole_decoupled.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/cartpole_decoupled/randomizer_cartpole_decoupled.py
@@ -13,74 +13,83 @@ class CartPoleDecoupledRandomizer:
             dr_cfg = yaml.full_load(f)
 
         self.dr_cfg = dr_cfg
-        self.custom_randomize = False
+        self.attribute_randomize = False
+        self.action_randomize = False
+        self.observation_randomize = False
         if self.dr_cfg is not None:
             if 'randomize' in self.dr_cfg['custom_domain_randomization']:
                 if self.dr_cfg['custom_domain_randomization']['randomize']:
-                    self.custom_randomize = True
-                    self.randomization_params = self.dr_cfg['custom_domain_randomization']['randomization_parameters']
+                    if self.dr_cfg['custom_domain_randomization']['attributes']:
+                        self.attribute_randomize = True
+                        self.attribute_params = self.dr_cfg['custom_domain_randomization']['attributes']
+                    if self.dr_cfg['custom_domain_randomization']['action']:
+                        self.action_randomize = True
+                        self.action_params = self.dr_cfg['custom_domain_randomization']['action']
+                    if self.dr_cfg['custom_domain_randomization']['observation']:
+                        self.observation_randomize = True
+                        self.observation_params = self.dr_cfg['custom_domain_randomization']['observation']
                 else:
-                    print("Custom domain set to 'False'.")
+                    print("Custom randomization is set to 'False'.")
             else:
                 print("Please specify if you would like to randomize.")
         else:
-            print("Custom domain randomization specification is empty.")
+            print("Custom randomization specifications are empty.")
 
         return
 
-    def randomizer(self, task):
+    def attribute_randomizer(self, task):
 
         custom_attributes = ['cart_mass', 'pole_len', 'pole_mass', 'pole_friction', 'moment_of_inertia', 'gravity']
 
         if self.custom_randomize:
-            if self.randomization_params is not None:
-                for attr in self.randomization_params:
+            if self.attribute_params is not None:
+                for attr in self.attribute_params:
                     if attr not in custom_attributes:
                         print(f'{attr} is not a valid attribute for the custom domain randomization.')
-                if 'cart_mass' in self.randomization_params:
-                    if 'bounds' in self.randomization_params['cart_mass']:
-                        low = self.randomization_params['cart_mass']['bounds'][0]
-                        high = self.randomization_params['cart_mass']['bounds'][1]
+                if 'cart_mass' in self.attribute_params:
+                    if 'bounds' in self.attribute_params['cart_mass']:
+                        low = self.attribute_params['cart_mass']['bounds'][0]
+                        high = self.attribute_params['cart_mass']['bounds'][1]
                         task.cart_mass *= ((low - high) * torch.rand(size=(task.num_envs,),
                                                    device=task.device) + high)
                     else:
                         print('Please specify bounds of randomization for cart mass.')
-                if 'pole_len' in self.randomization_params:
-                    if 'bounds' in self.randomization_params['pole_len']:
-                        low = self.randomization_params['pole_len']['bounds'][0]
-                        high = self.randomization_params['pole_len']['bounds'][1]
+                if 'pole_len' in self.attribute_params:
+                    if 'bounds' in self.attribute_params['pole_len']:
+                        low = self.attribute_params['pole_len']['bounds'][0]
+                        high = self.attribute_params['pole_len']['bounds'][1]
                         task.pole_len *= ((low - high) * torch.rand(size=(task.num_envs,),
                                                    device=task.device) + high)
                     else:
                         print('Please specify bounds of randomization for cart mass.')
-                if 'pole_mass' in self.randomization_params:
-                    if 'bounds' in self.randomization_params['pole_mass']:
-                        low = self.randomization_params['pole_mass']['bounds'][0]
-                        high = self.randomization_params['pole_mass']['bounds'][1]
+                if 'pole_mass' in self.attribute_params:
+                    if 'bounds' in self.attribute_params['pole_mass']:
+                        low = self.attribute_params['pole_mass']['bounds'][0]
+                        high = self.attribute_params['pole_mass']['bounds'][1]
                         task.pole_mass *= ((low - high) * torch.rand(size=(task.num_envs,),
                                                    device=task.device) + high)
                     else:
                         print('Please specify bounds of randomization for cart mass.')
-                if 'pole_friction' in self.randomization_params:
-                    if 'bounds' in self.randomization_params['pole_friction']:
-                        low = self.randomization_params['pole_friction']['bounds'][0]
-                        high = self.randomization_params['pole_friction']['bounds'][1]
+                if 'pole_friction' in self.attribute_params:
+                    if 'bounds' in self.attribute_params['pole_friction']:
+                        low = self.attribute_params['pole_friction']['bounds'][0]
+                        high = self.attribute_params['pole_friction']['bounds'][1]
                         task.pole_friction *= ((low - high) * torch.rand(size=(task.num_envs,),
                                                    device=task.device) + high)
                     else:
                         print('Please specify bounds of randomization for cart mass.')
-                if 'moment_of_inertia' in self.randomization_params:
-                    if 'bounds' in self.randomization_params['moment_of_inertia']:
-                        low = self.randomization_params['moment_of_inertia']['bounds'][0]
-                        high = self.randomization_params['moment_of_inertia']['bounds'][1]
+                if 'moment_of_inertia' in self.attribute_params:
+                    if 'bounds' in self.attribute_params['moment_of_inertia']:
+                        low = self.attribute_params['moment_of_inertia']['bounds'][0]
+                        high = self.attribute_params['moment_of_inertia']['bounds'][1]
                         task.moment_of_inertia *= ((low - high) * torch.rand(size=(task.num_envs,),
                                                    device=task.device) + high)
                     else:
                         print('Please specify bounds of randomization for cart mass.')
-                if 'gravity' in self.randomization_params:
-                    if 'bounds' in self.randomization_params['gravity']:
-                        low = self.randomization_params['gravity']['bounds'][0]
-                        high = self.randomization_params['gravity']['bounds'][1]
+                if 'gravity' in self.attribute_params:
+                    if 'bounds' in self.attribute_params['gravity']:
+                        low = self.attribute_params['gravity']['bounds'][0]
+                        high = self.attribute_params['gravity']['bounds'][1]
                         task.gravity *= ((low - high) * torch.rand(size=(task.num_envs,),
                                                    device=task.device) + high)
                     else:
@@ -90,5 +99,17 @@ class CartPoleDecoupledRandomizer:
 
         return
 
+    def action_randomizer(self, action):
+        if self.action_randomize:
+            print('Hello World')
+        else:
+            print('Bye World')
+        return
 
+    def observation_randomizer(self, observation):
+        if self.action_randomize:
+            print('Hello World')
+        else:
+            print('Bye World')
+        return
 
