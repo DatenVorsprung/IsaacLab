9a245d54a12c2e6634f404d39b5bdd725320c2f8
Clemens Blab <clemens@datenvorsprung.at>
2024-07-02 11:51:52 +0200

    Added more options to attribute randomization, added action and observation randomization, and improved existing custom randomization

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/cartpole_decoupled/randomizer_cartpole_decoupled.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/cartpole_decoupled/randomizer_cartpole_decoupled.py
@@ -39,61 +39,49 @@ class CartPoleDecoupledRandomizer:
 
     def attribute_randomizer(self, task):
 
-        custom_attributes = ['cart_mass', 'pole_len', 'pole_mass', 'pole_friction', 'moment_of_inertia', 'gravity']
+        dict_attributes = {'cart_mass': task.cart_mass, 'pole_len': task.pole_len, 'pole_mass': task.pole_mass,
+                           'pole_friction': task.pole_friction, 'moment_of_inertia': task.moment_of_inertia,
+                           'gravity': task.gravity}
+
+        custom_distributions = ['uniform', 'gaussian']
+        custom_operations = ['additive', 'scaling']
 
         if self.attribute_randomize:
             if self.attribute_params is not None:
-                for attr in self.attribute_params:
-                    if attr not in custom_attributes:
+                for attr in self.attribute_params.keys():
+                    if attr not in dict_attributes.keys():
                         print(f'{attr} is not a valid attribute for the custom domain randomization.')
-                if 'cart_mass' in self.attribute_params:
-                    if 'bounds' in self.attribute_params['cart_mass']:
-                        low = self.attribute_params['cart_mass']['bounds'][0]
-                        high = self.attribute_params['cart_mass']['bounds'][1]
-                        task.cart_mass *= ((low - high) * torch.rand(size=(task.num_envs,),
-                                                   device=task.device) + high)
-                    else:
-                        print('Please specify bounds of randomization for cart mass.')
-                if 'pole_len' in self.attribute_params:
-                    if 'bounds' in self.attribute_params['pole_len']:
-                        low = self.attribute_params['pole_len']['bounds'][0]
-                        high = self.attribute_params['pole_len']['bounds'][1]
-                        task.pole_len *= ((low - high) * torch.rand(size=(task.num_envs,),
-                                                   device=task.device) + high)
-                    else:
-                        print('Please specify bounds of randomization for cart mass.')
-                if 'pole_mass' in self.attribute_params:
-                    if 'bounds' in self.attribute_params['pole_mass']:
-                        low = self.attribute_params['pole_mass']['bounds'][0]
-                        high = self.attribute_params['pole_mass']['bounds'][1]
-                        task.pole_mass *= ((low - high) * torch.rand(size=(task.num_envs,),
-                                                   device=task.device) + high)
-                    else:
-                        print('Please specify bounds of randomization for cart mass.')
-                if 'pole_friction' in self.attribute_params:
-                    if 'bounds' in self.attribute_params['pole_friction']:
-                        low = self.attribute_params['pole_friction']['bounds'][0]
-                        high = self.attribute_params['pole_friction']['bounds'][1]
-                        task.pole_friction *= ((low - high) * torch.rand(size=(task.num_envs,),
-                                                   device=task.device) + high)
-                    else:
-                        print('Please specify bounds of randomization for cart mass.')
-                if 'moment_of_inertia' in self.attribute_params:
-                    if 'bounds' in self.attribute_params['moment_of_inertia']:
-                        low = self.attribute_params['moment_of_inertia']['bounds'][0]
-                        high = self.attribute_params['moment_of_inertia']['bounds'][1]
-                        task.moment_of_inertia *= ((low - high) * torch.rand(size=(task.num_envs,),
-                                                   device=task.device) + high)
-                    else:
-                        print('Please specify bounds of randomization for cart mass.')
-                if 'gravity' in self.attribute_params:
-                    if 'bounds' in self.attribute_params['gravity']:
-                        low = self.attribute_params['gravity']['bounds'][0]
-                        high = self.attribute_params['gravity']['bounds'][1]
-                        task.gravity *= ((low - high) * torch.rand(size=(task.num_envs,),
-                                                   device=task.device) + high)
-                    else:
-                        print('Please specify bounds of randomization for cart mass.')
+                for attr in dict_attributes.keys():
+                    if attr in self.attribute_params.keys():
+                        if not set(('operation', 'distribution', 'distribution_params')).issubset(
+                                self.attribute_params[attr].keys()):
+                            print('Please provide all ingredients for randomizing the action: operation, distribution, distribution_params.')
+                        else:
+                            if not self.action_params['distribution'] in custom_distributions:
+                                print('Please select one of the two distributions: uniform, gaussian.')
+                            else:
+                                if self.attribute_params[attr]['distribution'] == 'uniform':
+                                    low = self.attribute_params[attr]['distribution_params'][0]
+                                    high = self.attribute_params[attr]['distribution_params'][1]
+                                    attribute_noise = ((low - high) * torch.rand(size=dict_attributes[attr].shape, device=task.device) + high)
+                                    if not self.action_params['operation'] in custom_operations:
+                                        print('Please select one of the two operations: additive, scaling.')
+                                    else:
+                                        if self.attribute_params[attr]['operation'] == 'additive':
+                                            dict_attributes[attr] += attribute_noise
+                                        if self.attribute_params[attr]['operation'] == 'scaling':
+                                            dict_attributes[attr] *= attribute_noise
+                                if self.attribute_params[attr]['distribution'] == 'gaussian':
+                                    mean = self.attribute_params[attr]['distribution_params'][0]
+                                    sd = self.attribute_params[attr]['distribution_params'][1]
+                                    action_noise = torch.normal(mean=(mean * torch.ones(size=dict_attributes[attr].shape, device=task.device)), std=(sd * torch.ones(size=dict_attributes[attr].shape, device=task.device)))
+                                    if not self.attribute_params[attr]['operation'] in custom_operations:
+                                        print('Please select one of the two operations: additive, scaling.')
+                                    else:
+                                        if self.attribute_params[attr]['operation'] == 'additive':
+                                            dict_attributes[attr] += action_noise
+                                        if self.attribute_params[attr]['operation'] == 'scaling':
+                                            dict_attributes[attr] *= action_noise
             else:
                 print('No attributes are selected to be randomized.')
 
@@ -136,10 +124,41 @@ class CartPoleDecoupledRandomizer:
                                 action *= action_noise
         return action
 
-    def observation_randomizer(self, observation):
-        if self.action_randomize:
-            print('Hello World')
-        else:
-            print('Bye World')
-        return
+    def observation_randomizer(self, task, observation):
+
+        custom_distributions = ['uniform', 'gaussian']
+        custom_operations = ['additive', 'scaling']
+
+        if self.observation_randomize:
+            if not set(('operation', 'distribution', 'distribution_params')).issubset(self.observation_params.keys()):
+                print('Please provide all ingredients for randomizing the observation: operation, distribution, distribution_params.')
+            else:
+                if not self.observation_params['distribution'] in custom_distributions:
+                    print('Please select one of the two distributions: uniform, gaussian.')
+                else:
+                    if self.observation_params['distribution'] == 'uniform':
+                        low = self.observation_params['distribution_params'][0]
+                        high = self.observation_params['distribution_params'][1]
+                        observation_noise = ((low - high) * torch.rand(size=observation.shape, device=task.device) + high)
+                        if not self.observation_params['operation'] in custom_operations:
+                            print('Please select one of the two operations: additive, scaling.')
+                        else:
+                            if self.observation_params['operation'] == 'additive':
+                                observation += observation_noise
+                            if self.observation_params['operation'] == 'scaling':
+                                observation *= observation_noise
+
+                    if self.observation_params['distribution'] == 'gaussian':
+                        mean = self.observation_params['distribution_params'][0]
+                        sd = self.observation_params['distribution_params'][1]
+                        observation_noise = torch.normal(mean=(mean * torch.ones(size=observation.shape, device=task.device)), std=(sd * torch.ones(size=observation.shape, device=task.device)))
+                        if not self.observation_params['operation'] in custom_operations:
+                            print('Please select one of the two operations: additive, scaling.')
+                        else:
+                            if self.observation_params['operation'] == 'additive':
+                                observation += observation_noise
+                            if self.observation_params['operation'] == 'scaling':
+                                observation *= observation_noise
+        return observation
+
 
