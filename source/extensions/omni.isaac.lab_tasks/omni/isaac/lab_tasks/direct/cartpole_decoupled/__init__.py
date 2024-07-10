f08e4e413854609389b45ca700d03e6133a74235
Lukas Linauer <lukas@datenvorsprung.at>
2024-07-10 09:14:53 +0200

    Create rl_games SAC config for cartpole_decooupled, add it to sac entry point

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/cartpole_decoupled/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/cartpole_decoupled/__init__.py
@@ -23,7 +23,8 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": CartpoleDecoupledEnvCfg,
-        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rl_games_ppo_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rl_games_sac_cfg_entry_point": f"{agents.__name__}:rl_games_sac_cfg.yaml",
         "rsl_rl_cfg_entry_point": agents.rsl_rl_ppo_cfg.CartpoleDecoupledPPORunnerCfg,
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
         "sb3_ppo_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
